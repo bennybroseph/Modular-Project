8f6e793c09b072c2fef55c9cbac2c86f36ae8b00
Benjamin Odom <bennybroseph@gmail.com>
2016-06-15 17:02:36 -0500

    Initial Commit
    Added a script which will take in a component, a property to render, and
    a min and max value to adhere to. This will allow you to create a health
    bar, cooldown indicator, slider, whatever you'd like and have it
    visually represented as any filled image very easily.

---
+++ Assets/Scripts/Library/ExecuteInEditor.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;  // Required for all 'MonoBehavior's
+using UnityEditor;  // Required for 'EditorAplication'
+
+namespace Library    // namespace denoting that this is part of the tile mapping functionality inside the editor
+{
+    // Executes 'Start()', and 'Update()' while in the editor
+    [ExecuteInEditMode]
+    public abstract class ExecuteInEditor : MonoBehaviour
+    {
+        private bool m_EditorCompiling;   // Used to determine whether Unity is currently compiling
+
+        /// <summary>
+        /// Run automatically by Unity on 3 known conditions
+        /// 1. Unity starts up
+        /// 2. Unity enters play mode
+        /// 3. Unity exits play mode
+        /// </summary>
+        private void Start()
+        {
+            m_EditorCompiling = false;
+
+            // Unity is in play mode
+            if (EditorApplication.isPlaying)
+                OnGameStart();      // Run Game initialization
+            // Unity started up || Game stopped running
+            else
+                OnEditorStart();    // Run Editor initialization
+        }
+        /// <summary>
+        /// Run automatically by Unity on 2 known conditions
+        /// 1. Unity is in play mode. Happens every frame
+        /// 2. Unity is not in play mode. Happens when something changes in the scene
+        /// </summary>
+        private void Update()
+        {
+            if (EditorApplication.isPlaying)
+                OnGameUpdate();     // Game is updating
+            // Unity is not in play mode and it is compiling without us knowing
+            else if (EditorApplication.isCompiling && !m_EditorCompiling)
+                m_EditorCompiling = true;   // Unity is compiling
+            // Unity is not compiling, but it WAS compiling last we knew
+            else if (m_EditorCompiling)
+            {
+                m_EditorCompiling = false;
+                OnEditorStart();    // Run initialization code again
+            }
+            // Unity was not compiling last we knew
+            // A game object is currently selected and that 'gameObject' is this one or this 'gameObject's parent
+            else if (Selection.activeGameObject != null
+                    && (Selection.activeGameObject == gameObject || Selection.activeGameObject.transform.parent == transform))
+                OnEditorUpdateSelected(); // Run special update function for when this 'gameObject' is considered selected
+            // Nothing currently selected or what IS selected isn't this 'gameObject' or this 'gameObject's parent
+            else
+                OnEditorUpdate();
+        }
+
+        // Virtual Functions to be implemented by inheriting class
+        protected abstract void OnEditorStart();
+        protected abstract void OnEditorUpdateSelected();
+        protected abstract void OnEditorUpdate();
+
+        protected abstract void OnGameStart();
+        protected abstract void OnGameUpdate();
+    }
+}
---
+++ Assets/Scripts/ProportionRenderer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Library;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class ProportionRenderer : ExecuteInEditor
+{
+    [Serializable]
+    private class ValueContainer
+    {
+        public string valueName;
+        public Image valueImage;
+        public string minValue;
+        public string maxValue;
+
+        // 'internal' prevents variables from being show in the inspector unless Debug mode is enabled
+        internal PropertyInfo valueProperty;
+        internal bool hasProperty;
+
+        internal PropertyInfo minValueProperty;
+        internal bool hasMinProperty;
+        internal int minValueParse;
+        internal PropertyInfo maxValueProperty;
+        internal bool hasMaxProperty;
+        internal int maxValueParse;
+
+    }
+
+    [SerializeField]
+    private Component m_Parent;
+    [SerializeField]
+    private bool m_AutoUpdate = false;
+
+    [Space]
+    [SerializeField]
+    private List<ValueContainer> m_Values;
+
+    public Component parent
+    {
+        get { return m_Parent; }
+        set { m_Parent = value; }
+    }
+
+    private void OnValidate()
+    {
+        foreach (var value in m_Values)
+        {
+            int result;
+
+            if (int.TryParse(value.valueName, out result) == false && value.valueName != null)
+                value.valueProperty = m_Parent.GetType().GetProperty(value.valueName);
+            else
+                value.valueProperty = null;
+
+            if (int.TryParse(value.minValue, out result) == false && value.minValue != null)
+                value.minValueProperty = m_Parent.GetType().GetProperty(value.minValue);
+            else
+            {
+                value.minValueParse = result;
+                value.minValueProperty = null;
+            }
+
+            if (int.TryParse(value.maxValue, out result) == false && value.maxValue != null)
+                value.maxValueProperty = m_Parent.GetType().GetProperty(value.maxValue);
+            else
+            {
+                value.maxValueParse = result;
+                value.maxValueProperty = null;
+            }
+
+            value.hasProperty = value.valueProperty != null;
+            value.hasMinProperty = value.minValueProperty != null;
+            value.hasMaxProperty = value.maxValueProperty != null;
+        }
+    }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        if (m_Values == null)
+            m_Values = new List<ValueContainer>();
+
+        if (m_AutoUpdate && m_Values.Count > 0)
+            StartCoroutine(UpdateValues());
+    }
+
+    protected override void OnEditorStart()
+    {
+        Awake();
+    }
+    protected override void OnEditorUpdateSelected()
+    {
+        OnEditorUpdate();
+    }
+    protected override void OnEditorUpdate()
+    {
+        GetImages();
+        OnValidate();
+    }
+
+    protected override void OnGameStart() { }
+    protected override void OnGameUpdate() { }
+
+    private void GetImages()
+    {
+        var images = 
+            GetComponentsInChildren<Image>().
+                Where(x => x.transform.parent == transform && x.type == Image.Type.Filled).ToArray();
+
+        foreach (var image in images)
+        {
+            var foundImage = false;
+            foreach (var valueContainer in m_Values)
+            {
+                if (valueContainer.valueImage != image)
+                    continue;
+
+                foundImage = true;
+                break;
+            }
+            if (foundImage)
+                continue;
+
+            var newValue = new ValueContainer { valueImage = image };
+            m_Values.Add(newValue);
+        }
+
+        ValueContainer[] tempValueContainers = new ValueContainer[m_Values.Count];
+        m_Values.CopyTo(tempValueContainers);
+        foreach (var valueContainer in tempValueContainers)
+        {
+            var foundImage = false;
+            foreach (var image in images)
+            {
+                if (valueContainer.valueImage != image)
+                    continue;
+
+                foundImage = true;
+                break;
+            }
+            if (!foundImage)
+                m_Values.Remove(valueContainer);
+        }
+    }
+
+    [ContextMenu("List Properties")]
+    private void ListProperties()
+    {
+        if (m_Parent == null)
+        {
+            Debug.Log("Parent is currently set to null");
+            return;
+        }
+
+        foreach (var info in m_Parent.GetType().GetProperties())
+        {
+            Debug.Log(info);
+        }
+    }
+
+    private IEnumerator UpdateValues()
+    {
+        while (true)
+        {
+            foreach (var value in m_Values.Where(x => x.hasProperty && x.valueImage != null))
+            {
+                var parsedCurrentValue = (float)value.valueProperty.GetValue(m_Parent, null);
+
+                var parsedMinValue =
+                    value.hasMinProperty
+                        ? (float)value.minValueProperty.GetValue(m_Parent, null)
+                        : value.minValueParse;
+                var parsedMaxValue =
+                    value.hasMaxProperty
+                        ? (float)value.maxValueProperty.GetValue(m_Parent, null)
+                        : value.maxValueParse;
+
+                value.valueImage.fillAmount =
+                    (parsedCurrentValue - parsedMinValue) / (parsedMaxValue - parsedMinValue);
+            }
+            yield return null;
+        }
+    }
+}
