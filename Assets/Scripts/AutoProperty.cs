a156eb4078a016efc0f1b41ad1508c136811f56c
Benjamin Odom <bennybroseph@gmail.com>
2017-01-10 09:59:56 -0600

    AutoProperty
    Simple storage container to quickly create a property which allows for
    easy tracking of when the value inside changes. Uses UnityEvents when in
    Unity and C# events otherwise.

---
+++ Assets/Scripts/AutoProperty.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AutoProperty<T>
+{
+#if UNITY_5
+    [SerializeField]
+#endif
+    private T m_Data;
+
+#if UNITY_5
+    [SerializeField]
+    public UnityEvent<T> onChangeEvent;
+#else
+    public delegate void OnChangeDelegate(T value);
+
+    public event OnChangeDelegate onChangeEvent;
+#endif
+
+    public T value
+    {
+        get { return m_Data; }
+        set
+        {
+            m_Data = value;
+
+            if (onChangeEvent != null)
+                onChangeEvent.Invoke(value);
+        }
+    }
+}
