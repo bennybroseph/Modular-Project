00199cc839a3f6a85a5cb7de4525cd5a28b413ee
Benjamin Odom <bennybroseph@gmail.com>
2017-01-10 18:22:20 -0600

    OldSchoolRotation
    Added Unity-Chan for testing.
    
    Added an old school looking effect to 3D models that attatch the script.
    Emulates a pre-rendered 3D model to sprite sheet effect. Looks like FF:
    Tactics or Mario Kart 64. Not complete.

---
+++ Assets/Scripts/OldSchoolRotation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class OldSchoolRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_ReferenceTransform;
+
+    private GameObject m_AnchorObject;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        m_AnchorObject = new GameObject("New Anchor");
+
+        m_AnchorObject.transform.forward = m_ReferenceTransform.forward;
+        transform.SetParent(m_AnchorObject.transform, false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_AnchorObject.transform.forward = m_ReferenceTransform.forward;
+        //transform.forward = m_ReferenceTransform.forward;
+
+        var newRotationY = m_ReferenceTransform.eulerAngles.y;
+        newRotationY += (int)((newRotationY + 22.5f)/ 45f) * -45f;
+
+        m_AnchorObject.transform.eulerAngles = new Vector3(0, newRotationY, 0);
+
+        //transform.forward = new Vector3();
+    }
+}
