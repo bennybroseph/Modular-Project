bd2a582d5d5e2bdd45bf06d84c7ae8bd7801e9dd
Benjamin Odom <bennybroseph@gmail.com>
2017-01-25 15:55:45 -0600

    Load Screen
    Simple Loading screen script which loads a level asynchronousand fills
    an image based on it's progress.

---
+++ Assets/Scripts/Library/SceneLoader.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Library
+{
+    using System.Collections;
+
+    using UnityEngine;
+
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private string m_SceneName;
+        [SerializeField]
+        private Image m_Image;
+
+        [SerializeField, Range(0f, 1f)]
+        private float m_LoadingProgress;
+
+        public float loadingProgress { get { return m_LoadingProgress; } }
+
+        // Use this for initialization
+        private void Awake()
+        {
+            StartCoroutine(WaitForLoadScene());
+        }
+
+        // Update is called once per frame
+        private IEnumerator WaitForLoadScene()
+        {
+            while (true)
+            {
+                while (!Input.GetKeyDown(KeyCode.Return))
+                    yield return null;
+
+                var asyncOperation = SceneManager.LoadSceneAsync(m_SceneName);
+
+                while (!asyncOperation.isDone)
+                {
+                    m_LoadingProgress = asyncOperation.progress;
+
+                    if (m_Image != null)
+                        m_Image.fillAmount = m_LoadingProgress;
+
+                    yield return null;
+                }
+
+                m_LoadingProgress = 1f;
+
+                yield return null;
+            }
+        }
+    }
+}
